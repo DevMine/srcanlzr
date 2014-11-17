bc48a8fafc5292f6c7cdac7421ce5827e112cd32
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-11-17 09:23:23 +0100

    test: add tests for the anlzr package

---
+++ anlzr/anlzr_test.go
@@ -0,0 +1,60 @@
+// Copyright 2014 The project AUTHORS. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package anlzr_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/DevMine/srcanlzr/anlzr"
+	"github.com/DevMine/srcanlzr/src"
+)
+
+var testdata = os.Getenv("GOPATH") + "/src/github.com/DevMine/srcanlzr/testdata/go.json"
+
+func TestRunAnalyzers(t *testing.T) {
+	bs, err := ioutil.ReadFile(testdata)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := new(src.Project)
+	if err := json.Unmarshal(bs, p); err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := anlzr.RunAnalyzers(p, anlzr.LoC{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if fmt.Sprintf("%.3f", res.AverageFuncLen) != "7.129" {
+		t.Errorf("average_function_length: expected 7.129, found %.3f",
+			res.AverageFuncLen)
+	}
+
+	if res.MinFuncLen != 1 {
+		t.Errorf("min_function_length: expected 1, found %d",
+			res.MinFuncLen)
+	}
+
+	if res.MaxFuncLen != 413 {
+		t.Errorf("max_function_length: expected 413, found %d",
+			res.MaxFuncLen)
+	}
+
+	if res.MedianFuncLen != 4 {
+		t.Errorf("max_function_length: expected 4, found %d",
+			res.MedianFuncLen)
+	}
+
+	if res.TotalLoC != 111945 {
+		t.Errorf("total_loc: expected 111945, found %d",
+			res.TotalLoC)
+	}
+}
