b89f9776b746e0654148f2ec7123858ff28ef8d2
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-12-23 12:56:59 +0100

    doc: document src.Package

--- src/package.go
+++ src/package.go
@@ -4,10 +4,21 @@
 
 package src
 
+// A package is a folder contaning at least one source file.
 type Package struct {
-	Name        string        `json:"name"`         // Package name
-	Path        string        `json:"path"`         // Package location
-	Doc         string        `json:"doc"`          // Package doc comments
-	SourceFiles []*SourceFile `json:"source_files"` // Source files
-	LoC         int64         `json:"loc"`          // Lines of Code
+	// The name of the pacakge (the folder name)
+	Name string `json:"name"`
+
+	// The full path of the package. The path must be relative to the root of
+	// the project and never be an absolute path.
+	Path string `json:"path"`
+
+	// The package documentation.
+	Doc string `json:"doc"`
+
+	// The list of all source files contained in the package.
+	SourceFiles []*SourceFile `json:"source_files"`
+
+	// The total number of lines of code.
+	LoC int64 `json:"loc"`
 }
