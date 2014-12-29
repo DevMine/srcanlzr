55769f6665350ac186b9e1b340e05ce455aa5f10
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-12-29 16:46:07 +0100

    src: add some omitempty annotations

--- src/source_file.go
+++ src/source_file.go
@@ -13,34 +13,34 @@ type SourceFile struct {
 	ProgLang *Language `json:"language"`
 
 	// List of the imports used by the srouce file.
-	Imports []string `json:"imports"`
+	Imports []string `json:"imports,omitempty"`
 
 	// Types definition
 	// TODO rename JSON key into type_definition
-	TypeDefs []TypeDef `json:"type_defs"`
+	TypeDefs []TypeDef `json:"type_defs,omitempty"`
 
 	// Structures definition
 	// TODO rename JSON key into structures
-	Structs []StructuredType `json:"structs"`
+	Structs []StructuredType `json:"structs,omitempty"`
 
 	// List of constants defined at the file level (e.g. global constants)
-	Constants []Constant `json:"constants"`
+	Constants []Constant `json:"constants,omitempty"`
 
 	// List of variables defined at the file level (e.g. global variables)
-	Variables []Variable `json:"variables"`
+	Variables []Variable `json:"variables,omitempty"`
 
 	// List of functions
-	Functions []Function `json:"functions"`
+	Functions []Function `json:"functions,omitempty"`
 
 	// List of interfaces
-	Interfaces []Interface `json:"interfaces"`
+	Interfaces []Interface `json:"interfaces,omitempty"`
 
 	// List of classes
-	Classes []Class `json:"classes"`
+	Classes []Class `json:"classes,omitempty"`
 
 	// List of traits
 	// See http://en.wikipedia.org/wiki/Trait_%28computer_programming%29
-	Traits []Trait `json:"traits"`
+	Traits []Trait `json:"traits,omitempty"`
 
 	// The total number of lines of code.
 	LoC int64 `json:"loc"`
