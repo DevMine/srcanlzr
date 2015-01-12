8e161401fda19bed6f4e991d0d1f59d574547866
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-01-12 17:11:36 +0100

    src: add unary expression

--- src/unary_expr.go
+++ src/unary_expr.go
@@ -4,13 +4,48 @@
 
 package src
 
+import "fmt"
+
+// Unary operators
+const (
+	NOT  = "NOT"
+	ADDR = "ADDR" // memory address (&foo)
+	STAR = "STAR" // dereference operator (*foo)
+)
+
 type UnaryExpr struct {
+	ExprName string `json:"expression_name"`
+	Op       string `json:"operator"` // operator
+	X        Expr   `json:"operand"`  // operand
 }
 
 func newUnaryExpr(m map[string]interface{}) (*UnaryExpr, error) {
-	//var err error
-	//errPrefix := "src/decl_stmt"
+	var err error
+	errPrefix := "src/unary_expr"
 	unaryexpr := UnaryExpr{}
+
+	if typ, err := extractStringValue("expression_name", errPrefix, m); err != nil {
+		// XXX It is not possible to add debug info on this error because it is
+		// required that this error be en "errNotExist".
+		return nil, errNotExist
+	} else if typ != UnaryExprName {
+		return nil, fmt.Errorf("invalid type: expected 'UnaryExpr', found '%s'", typ)
+	}
+
+	unaryexpr.ExprName = UnaryExprName
+
+	if unaryexpr.Op, err = extractStringValue("operator", errPrefix, m); err != nil {
+		return nil, addDebugInfo(err)
+	}
+
+	exprMap, err := extractMapValue("operand", errPrefix, m)
+	if err != nil {
+		return nil, addDebugInfo(err)
+	}
+
+	if unaryexpr.X, err = newExpr(exprMap); err != nil {
+		return nil, addDebugInfo(err)
+	}
 
 	return &unaryexpr, nil
 }
