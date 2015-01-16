ce71c80e1973b037d1f38377fa2e09a6016aed68
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-01-16 17:08:11 +0100

    anlzr: finalize cyclomatic complexity

--- anlzr/complexity.go
+++ anlzr/complexity.go
@@ -4,11 +4,7 @@
 
 package anlzr
 
-import (
-	"fmt"
-
-	"github.com/DevMine/srcanlzr/src"
-)
+import "github.com/DevMine/srcanlzr/src"
 
 type Complexity struct{}
 
@@ -30,14 +26,14 @@ func (c Complexity) Analyze(p *src.Project, r *Result) error {
 				numFuncs++
 				fileComplexity += functionCyclomaticComplexity(f)
 
-				for _, stmt := range f.Body {
+				/*for _, stmt := range f.Body {
 					switch stmt.(type) {
 					case src.IfStmt, src.LoopStmt:
 						fileComplexity++
 					}
 				}
 
-				fileComplexity += int64(len(f.Type.Results))
+				fileComplexity += int64(len(f.Type.Results))*/
 			}
 
 			for _, cls := range sf.Classes {
@@ -45,15 +41,14 @@ func (c Complexity) Analyze(p *src.Project, r *Result) error {
 					numFuncs++
 					fileComplexity += methodCyclomaticComplexity(m)
 
-					for _, stmt := range m.Body {
+					/*for _, stmt := range m.Body {
 						switch stmt.(type) {
 						case src.IfStmt, src.LoopStmt:
 							fileComplexity++
 						}
 					}
 
-					fileComplexity += int64(len(m.Type.Results))
-
+					fileComplexity += int64(len(m.Type.Results))*/
 				}
 			}
 
@@ -62,15 +57,14 @@ func (c Complexity) Analyze(p *src.Project, r *Result) error {
 					numFuncs++
 					fileComplexity += methodCyclomaticComplexity(m)
 
-					for _, stmt := range m.Body {
+					/*for _, stmt := range m.Body {
 						switch stmt.(type) {
 						case src.IfStmt, src.LoopStmt:
 							fileComplexity++
 						}
 					}
 
-					fileComplexity += int64(len(m.Type.Results))
-
+					fileComplexity += int64(len(m.Type.Results))*/
 				}
 			}
 
@@ -95,7 +89,7 @@ func functionCyclomaticComplexity(f *src.FuncDecl) int64 {
 	cc := int64(1) // cyclomatic complexity
 
 	for _, s := range f.Body {
-		cc += statementComplexity(&s)
+		cc += statementComplexity(s)
 	}
 
 	return cc
@@ -105,34 +99,105 @@ func methodCyclomaticComplexity(m *src.MethodDecl) int64 {
 	cc := int64(1) // cyclomatic complexity
 
 	for _, s := range m.Body {
-		cc += statementComplexity(&s)
+		cc += statementComplexity(s)
 	}
 
 	return cc
+}
+
+func exprComplexity(e src.Expr) int64 {
+	var c int64
+
+	switch e.(type) {
+	case *src.BinaryExpr:
+		be := e.(*src.BinaryExpr)
+		// TODO check the operator
+		c += exprComplexity(be.LeftExpr)
+		c += exprComplexity(be.RightExpr)
+	}
+
+	return c
 }
 
 func statementComplexity(s src.Stmt) int64 {
 	var c int64
 
 	switch s.(type) {
-	case src.IfStmt:
-		fmt.Println("foo")
+	case *src.IfStmt:
 		c++
 
-		stmt := s.(src.IfStmt)
+		is := s.(*src.IfStmt)
 
-		for _, s := range stmt.Body {
+		if is.Cond != nil {
+			c += exprComplexity(is.Cond)
+		}
+
+		for _, s := range is.Body {
 			c += statementComplexity(s)
 		}
-	case src.LoopStmt:
-		fmt.Println("bar")
+
+		if is.Else != nil && len(is.Else) > 0 {
+			for _, s := range is.Else {
+				c += statementComplexity(s)
+			}
+		}
+	case *src.LoopStmt:
 		c++
 
-		stmt := s.(src.LoopStmt)
+		ls := s.(*src.LoopStmt)
 
-		for _, s := range stmt.Body {
+		if ls.Cond != nil {
+			c += exprComplexity(ls.Cond)
+		}
+
+		for _, s := range ls.Body {
 			c += statementComplexity(s)
 		}
+
+		if ls.Else != nil && len(ls.Else) > 0 {
+			for _, s := range ls.Else {
+				c += statementComplexity(s)
+			}
+		}
+	case *src.RangeLoopStmt:
+		c++
+
+		rls := s.(*src.RangeLoopStmt)
+
+		for _, s := range rls.Body {
+			c += statementComplexity(s)
+		}
+	case *src.SwitchStmt:
+		c++
+
+		ss := s.(*src.SwitchStmt)
+
+		if ss.Cond != nil {
+			c += exprComplexity(ss.Cond)
+		}
+
+		if ss.CaseClauses != nil {
+			for _, cc := range ss.CaseClauses {
+				c++
+
+				for _, cond := range cc.Conds {
+					if cond != nil {
+						c += exprComplexity(cond)
+					}
+				}
+
+				for _, s := range cc.Body {
+					c += statementComplexity(s)
+				}
+			}
+		}
+
+		if ss.Default != nil && len(ss.Default) > 0 {
+			for _, s := range ss.Default {
+				c += statementComplexity(s)
+			}
+		}
+
 	}
 
 	return c
