59cfe936fb4e8f78f987b88fcdda638458d08dda
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-05-30 18:26:45 +0200

    src/decoder: add more tests

--- src/decode_test.go
+++ src/decode_test.go
@@ -6,6 +6,7 @@ package src
 
 import (
 	"bytes"
+	"errors"
 	"testing"
 )
 
@@ -79,6 +80,160 @@ func TestDecodeLanguage(t *testing.T) {
 		t.Fatal(dec.err)
 	}
 	compareLanguages(t, lang, expected)
+}
+
+func TestUnmarshalString(t *testing.T) {
+	dec := newDecoder(new(bytes.Buffer))
+	if str, err := dec.unmarshalString([]byte("foo")); err != nil {
+		t.Error(err)
+	} else if str != "foo" {
+		t.Errorf("unmarshalString 'foo': found '%s', expected 'foo'", str)
+	}
+
+	expectedErr := errors.New("unable to unmarshal string: data is nil")
+	if _, err := dec.unmarshalString(nil); err == nil {
+		t.Errorf("unmarshalString nil: found no error, expected \"%v\"", expectedErr)
+	} else if err.Error() != expectedErr.Error() {
+		t.Errorf("unmarshalString nil: found error \"%v\", expected \"%v\"", err, expectedErr)
+	}
+}
+
+func TestAssertNewObject(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"{": returnStatus{true, nil},
+		"":  returnStatus{false, errors.New("expected value, found EOF")},
+		"[": returnStatus{false, errors.New("expected object, found '['")},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.assertNewObject(); status != ret.status {
+			t.Errorf("assertNewObject '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("assertNewObject '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
+}
+
+func TestAssertNewArray(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"[": returnStatus{true, nil},
+		"":  returnStatus{false, errors.New("expected value, found EOF")},
+		"{": returnStatus{false, errors.New("expected array, found '{'")},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.assertNewArray(); status != ret.status {
+			t.Errorf("assertNewArray '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("assertNewArray '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
+}
+
+func TestIsEndObject(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"}":  returnStatus{true, nil},
+		",":  returnStatus{false, nil},
+		"42": returnStatus{false, errors.New("expected 'comma', found 'integer literal'")},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.isEndObject(); status != ret.status {
+			t.Errorf("isEndObject '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("isEndObject '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
+}
+
+func TestIsEndArray(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"]":  returnStatus{true, nil},
+		",":  returnStatus{false, nil},
+		"42": returnStatus{false, errors.New("expected 'comma', found 'integer literal'")},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.isEndArray(); status != ret.status {
+			t.Errorf("isEndArray '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("isEndArray '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
+}
+
+func TestIsEmptyObject(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"}":        returnStatus{true, nil},
+		"":         returnStatus{false, errors.New("unexpected EOF")},
+		"\"foo\":": returnStatus{false, nil},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.isEmptyObject(); status != ret.status {
+			t.Errorf("isEmptyObject '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("isEmptyObject '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
+}
+
+func TestIsEmptyArray(t *testing.T) {
+	type returnStatus struct {
+		status bool
+		err    error
+	}
+	input := map[string]returnStatus{
+		"]":  returnStatus{true, nil},
+		"":   returnStatus{false, errors.New("unexpected EOF")},
+		"42": returnStatus{false, nil},
+	}
+
+	for in, ret := range input {
+		buf := bytes.NewBufferString(in)
+		dec := newDecoder(buf)
+		if status := dec.isEmptyArray(); status != ret.status {
+			t.Errorf("isEmptyArray '%s': found %v, expected %v", in, status, ret.status)
+		}
+		if err := dec.err; err != nil && err.Error() != ret.err.Error() {
+			t.Errorf("isEmptyArray '%s': found error \"%v\", expected error \"%v\"", in, dec.err, ret.err)
+		}
+	}
 }
 
 func compareLanguages(t *testing.T, found, expected *Language) {
