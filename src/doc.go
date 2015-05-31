5c6a7fb462dba9955168ac3e76d0ee7b82eb0f17
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-05-31 13:26:28 +0200

    src/doc: add more documentation for the src package

--- src/doc.go
+++ src/doc.go
@@ -7,17 +7,27 @@
 	indepently of the language. In other words, it provides a generic
 	representation (abstraction) of a source code.
 
+
+	Lines of Code Counting
+
 	The number of real lines of code must be precomputed by the language
-	parsers. This is the only "feature" that must be precomputed because it is
-	used by srctool (https://github.com/DevMine/srctool) to eliminate empty
-	packages. Since this information is already calculated, the source analyzers
-	won't re-count. They will just use the total as it is. Therefore, it must be
-	accurate.
+	parsers. This is the only "feature" that must be precomputed because it may
+	have multiple usages:
+
+	1. Eliminate empty projects
+	2. Evalutate project size
+	3. Verify that the decoding is correct
+	4. Normalize various counts
+	5. ...
+
+	Therefore, this count must be accurate and strictly follow the following
+	rules:
 
 	We only count statements and declarations as a line of code. Comments,
 	package declaration, imports, expression, etc. must not be taken into
 	account. Since an exemple is worth more than a thousand words, let's
 	condider the following snippet:
+
 	   // Package doc (does not count as a line of code)
 	   package main // does not count as a line of code
 
@@ -30,6 +40,39 @@
 	   }
 
 	The expected number of lines of code is 2: The main function declaration
-	and the call to the fmt.Println function.
+	and the call to fmt.Println function.
+
+
+	Performance
+
+	DevMine project is dealing with Terabytes of source code, therefore the
+	JSON decoding must be efficient. That is why we implemented our own JSON
+	decoder that focuses on performance. To do so, we had to make some
+	choices and add some constraints for language parsers in order to make this
+	process as fast as possible.
+
+	JSON is usually unpredicatable which forces JSON parsers to be generic to
+	deal with every possible kind of input. In DevMine, we have a well defined
+	structure, thus instead of writting a generic JSON decoder we wrote one that
+	decodes only src.Project objects. This really improves the performances
+	since we don't need to use reflextion, generic types (interface{}) and type
+	assertion. The drawback of this choice is that we have to update the decoder
+	everytime we modify our structures.
+
+	Most JSON parsers assume that the JSON input is potentially invalid
+	(ie. malformed). We don't. Unlike json.Unmarshal, we don't Check for
+	well-formedness.
+
+	We also force the language parsers to put the "expression_name" and
+	"statement_name" fields at the beginning of the JSON object. We use that
+	convention to decode generic ast.Expr and ast.Stmt without reading the whole
+	JSON object.
+
+
+	Further improvements
+
+	The code became quite repetitive. Since most of the logic has been
+	encapsulated into helper methods, it would be really nice to generate the
+	decoding methods using "go generate".
 */
 package src
