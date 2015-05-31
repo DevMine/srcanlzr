a2ab3e7cf2858cb7e125717d81561505d578bc4d
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2015-05-31 19:51:26 +0200

    src/token: improve doc

--- src/token/token.go
+++ src/token/token.go
@@ -4,24 +4,25 @@
 
 package token
 
+// Literals
 const (
-	IntLit    = "INT"
-	FloatLit  = "FLOAT"
-	ImagLit   = "IMAG"
-	CharLit   = "CHAR"
-	StringLit = "STRING"
-	BoolLit   = "BOOl"
-	NilLit    = "NIL"
+	IntLit    = "INT"    // integer (1, 2, 3, ...)
+	FloatLit  = "FLOAT"  // floatint point number (1.2)
+	ImagLit   = "IMAG"   // imaginary number (complex number)
+	CharLit   = "CHAR"   // character ('a', 'b', 'c', ...)
+	StringLit = "STRING" // string ("foo")
+	BoolLit   = "BOOl"   // boolean (true/false)
+	NilLit    = "NIL"    // null value (nil, null, NULL, ...)
 )
 
 // Binary operators
 const (
 	// numerical operators
-	ADD = "ADD"
-	SUB = "SUB"
-	MUL = "MUL"
-	QUO = "QUO"
-	MOD = "MOD"
+	ADD = "ADD" // addition
+	SUB = "SUB" // subtraction
+	MUL = "MUL" // multiplication
+	QUO = "QUO" // division
+	MOD = "MOD" // modulo
 
 	// logical operators
 	AND     = "AND"         // binary and (&)
@@ -48,68 +49,73 @@ const (
 	VarDecl   = "VAR"      // variable
 )
 
+// Expression names
 const (
-	UnaryExprName           = "UNARY"
-	BinaryExprName          = "BINARY"
-	TernaryExprName         = "TERNARY"
-	IncDecExprName          = "INC_DEC"
-	CallExprName            = "CALL"
-	ConstructorCallExprName = "CONSTRUCTOR_CALL"
-	ArrayExprName           = "ARRAY"
-	IndexExprName           = "INDEX"
+	// Expression names
+	UnaryExprName           = "UNARY"            // unary expression (!foo)
+	BinaryExprName          = "BINARY"           // binary expression (foo && bar)
+	TernaryExprName         = "TERNARY"          // ternary expression (foo ? 42 : 0)
+	IncDecExprName          = "INC_DEC"          // increment/decrement expression (i++/i--)
+	CallExprName            = "CALL"             // call expression (foo(42))
+	ConstructorCallExprName = "CONSTRUCTOR_CALL" // constructor call expression (foo = new Foo())
+	ArrayExprName           = "ARRAY"            // array expression
+	IndexExprName           = "INDEX"            // index expression (foo[i])
 
-	BasicLitName = "BASIC_LIT"
-	FuncLitName  = "FUNC_LIT"
-	ClassLitName = "CLASS_LIT"
-	ArrayLitName = "ARRAY_LIT"
+	// Literal names
+	BasicLitName = "BASIC_LIT" // basic literal ("foo", 42, 12.34, ...)
+	FuncLitName  = "FUNC_LIT"  // function literal (foo := func() { ... })
+	ClassLitName = "CLASS_LIT" // class literal
+	ArrayLitName = "ARRAY_LIT" // array literal (foo = [1,2,3])
 
-	StructTypeName = "STRUCT_TYPE"
+	// Type names
+	StructTypeName = "STRUCT_TYPE" // struct type
 
-	AttrRefName = "ATTR_REF"
-
-	ValueSpecName = "VALUE_SPEC"
-
-	IdentName = "IDENT"
+	// Other
+	AttrRefName   = "ATTR_REF"   // attribute reference (this.foo)
+	ValueSpecName = "VALUE_SPEC" // value specifier
+	IdentName     = "IDENT"      // identifier
 )
 
 // Increment/Decrement operators
 const (
-	INC = "INC"
-	DEC = "DEC"
+	INC = "INC" // increment operator (i++)
+	DEC = "DEC" // decreement operator (i--)
 )
 
 // Unary operators
 const (
-	NOT  = "NOT"
+	NOT  = "NOT"  // negation operator
 	ADDR = "ADDR" // memory address (&foo)
 	STAR = "STAR" // dereference operator (*foo)
 
-	NEG = "NEG"
-	POS = "POS"
+	NEG = "NEG" // negative sign (-)
+	POS = "POS" // positive sign (+)
 )
 
+// Statement names
 const (
-	IfStmtName        = "IF"
-	SwitchStmtName    = "SWITCH"
-	LoopStmtName      = "LOOP"
-	RangeLoopStmtName = "RANGE_LOOP"
-	AssignStmtName    = "ASSIGN"
-	DeclStmtName      = "DECL"
-	ReturnStmtName    = "RETURN"
-	ExprStmtName      = "EXPR"
-	TryStmtName       = "TRY"
-	ThrowStmtName     = "THROW"
-	OtherStmtName     = "OTHER"
+	IfStmtName        = "IF"         // if statement
+	SwitchStmtName    = "SWITCH"     // switch statement
+	LoopStmtName      = "LOOP"       // loop statement (for, while, do...while, ...)
+	RangeLoopStmtName = "RANGE_LOOP" // range loops( for foo := range bar, for foo in bar, ...)
+	AssignStmtName    = "ASSIGN"     // assign statement (foo = bar)
+	DeclStmtName      = "DECL"       // declaration statement (int foo = 42, foo := 42, ...)
+	ReturnStmtName    = "RETURN"     // return statement (return 42)
+	ExprStmtName      = "EXPR"       // expression statement
+	TryStmtName       = "TRY"        // try...catch statement
+	ThrowStmtName     = "THROW"      // throw exception
+	OtherStmtName     = "OTHER"      // any other not supported statement
 )
 
 // Supported visiblities
 const (
-	PublicVisibility    = "public"
-	PackageVisibility   = "package"
-	ProtectedVisibility = "protected"
-	PrivateVisibility   = "private"
+	PublicVisibility    = "public"    // public (visible from everywhere)
+	PackageVisibility   = "package"   // package (visible from every entity inside the package)
+	ProtectedVisibility = "protected" // protected (only visible from subclasses)
+	PrivateVisibility   = "private"   // private
 )
 
+// list of all supported visibilities
 var suppVisibility = []string{
 	PublicVisibility,
 	PackageVisibility,
@@ -119,10 +125,10 @@ var suppVisibility = []string{
 
 // Type names
 const (
-	TypeMapName         = "MAP"
-	TypeStructName      = "STRUCT"
-	TypeArrayName       = "ARRAY"
-	TypeFuncName        = "FUNC"
-	TypeInterfaceName   = "INTERFACE"
-	TypeUnsupportedName = "UNSUPPORTED"
+	TypeMapName         = "MAP"         // hash map
+	TypeStructName      = "STRUCT"      // structure
+	TypeArrayName       = "ARRAY"       // array
+	TypeFuncName        = "FUNC"        // function
+	TypeInterfaceName   = "INTERFACE"   // interface
+	TypeUnsupportedName = "UNSUPPORTED" // unsupported type (for error)
 )
